cd43bbc715c7e24976a54cc61c1c43b68de2676b
Luccas Santos <luccaselias0@gmail.com>
2025-10-31 09:43:47 -0300

    test(conftest, test_agenda): update on tests and on test variables for greater  accuracy

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,22 +16,23 @@ def agenda():
     Cria uma instância de Agenda que persiste por todo o módulo de teste.
     Útil para cenários onde a inserção de dados deve acumular.
     """
-    agenda = Agenda()
+    agenda = Agenda(testing=True)
     yield agenda  
 
 
 CASES_SUCCESS = [
-        ("Teste A", "32132132112"),
-        ("Teste B", "12312312312"),
-        ("Teste C", "12312312312"),
-        ("Teste D", "123sdadaddc45sdadadwds678dsadasdwds09"),
+        ("Teste A", "32132132112", "testea@email.com"),
+        ("Teste B", "12312312312", "testeb@gmail.com"),
+        ("Teste C", "12312312312", "testec@net.co"),
+        ("Teste D", "123sdadaddc45sdadadwds678dsadasdwds09", "testea@yahoo.com.br"),
     ]
 
 CASES_FAIL = [
-        ("", "32132132112"),
-        ("Teste A", ""),
-        ("Teste B", "12312"),
-        ("Teste C", "dsadwvc"),
+        ("", "32132132112", "testea@email.com"),
+        ("Teste AA", "", "testea@email.com"),
+        ("Teste BB", "12312", "testea@email.com"),
+        ("Teste CC", "dsadwvc", "testea@email.com"),
+        ("Teste DD", "12312312332", "")
     ]
 
 
@@ -41,9 +42,10 @@ CASES_UPDATE_SUCCESS = [
 ]
 CASES_UPDATE_FAIL = [
     ("","32112312332"),
-    ("Teste c","12332")
+    ("Teste C","12332"),
+    ("Teste T", "12312312322")
 ]
 
 
-CASES_DELETE_SUCCESS = []
-CASES_DELETE_FAIL = []
+CASES_DELETE_SUCCESS = [("Teste C"),("Teste A")]
+CASES_DELETE_FAIL = [(""),("Te"),("213"),(123)]
--- tests/test_agenda.py
+++ tests/test_agenda.py
@@ -1,9 +1,9 @@
 import pytest
 from conftest import CASES_SUCCESS, CASES_FAIL, CASES_UPDATE_SUCCESS, CASES_UPDATE_FAIL, CASES_DELETE_SUCCESS, CASES_DELETE_FAIL
 
-@pytest.mark.parametrize("name,number", CASES_SUCCESS)
-def test_add(agenda, monkeypatch, name:str, number):
-    input_list = [name, number]
+@pytest.mark.parametrize("name,number, email", CASES_SUCCESS)
+def test_add(agenda, monkeypatch, name:str, number, email):
+    input_list = [name, number, email]
 
     def mock_input(_):
         if not input_list:
@@ -15,18 +15,19 @@ def test_add(agenda, monkeypatch, name:str, number):
 
     assert agenda_insert == 201
     assert agenda.show_one("Luccas") == 404
-    assert any(name.capitalize() in contanct for contanct in agenda.contatos)
+    assert name in [x.get('name') for x in agenda.contatos]
     assert len(agenda.contatos) != 0
 
 
-@pytest.mark.parametrize("name,number", CASES_FAIL )
-def test_add_fail(agenda,monkeypatch,name,number):
-    input_list = [name,number]
+@pytest.mark.parametrize("name,number, email", CASES_FAIL )
+def test_add_fail(agenda,monkeypatch,name,number, email):
+    input_list = [name,number, email]
     def mock_input(_):
         return input_list.pop(0)
     
     monkeypatch.setattr('builtins.input',mock_input)
     agenda_insert = agenda.add()
+    agenda.reset_state()
     print(agenda.contatos)
     assert agenda_insert in [401,400,404]
 
@@ -39,7 +40,7 @@ def test_update(agenda,monkeypatch,name,number):
 
     assert agenda.update() == 204
 
-@pytest.mark.parametrize("name", [("Teste b"),("Teste b")])
+@pytest.mark.parametrize("name", [("Teste B"),("Teste B")])
 def test_update_favorite(agenda,monkeypatch, name):
     input_list = [name]
     def mock_input(_):
@@ -54,5 +55,27 @@ def test_update_fail(agenda,monkeypatch,name,number):
     def mock_input(_):
         return input_list.pop(0)
     monkeypatch.setattr('builtins.input', mock_input)
-    print(agenda.contatos)
-    assert agenda.update() in [400,401]
+    agenda.reset_state()
+
+    assert agenda.update() in [400,401,404]
+    
+
+@pytest.mark.parametrize("name", CASES_DELETE_SUCCESS)
+def test_delete(agenda, monkeypatch, name):
+    input_list = [name]
+    def mock_input(_):
+        return input_list.pop(0)
+    monkeypatch.setattr('builtins.input', mock_input)
+    agenda.reset_state()
+    assert agenda.delete() == 204
+    
+@pytest.mark.parametrize('name', CASES_DELETE_FAIL)
+def test_delete_fail(agenda, monkeypatch, name):
+    input_list = [name]
+    def mock_input(_):
+        return input_list.pop(0)
+    monkeypatch.setattr('builtins.input', mock_input)
+    
+    agenda.reset_state()
+
+    assert agenda.delete() in [401, 404]
